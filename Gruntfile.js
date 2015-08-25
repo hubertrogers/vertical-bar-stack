a661c98736667ae4933d644c3d002706b17a318c
hubertrogers <hubert.rogers@gmail.com>
2015-08-25 15:01:22 +0100

    fixed dist handling in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -194,7 +194,6 @@ module.exports = function (grunt) {
           '<%= config.dist %>/scripts/{,*/}*.js',
           '<%= config.dist %>/styles/{,*/}*.css',
           '<%= config.dist %>/images/{,*/}*.*',
-          '<%= config.dist %>/data/{,*/}*.*',
           '<%= config.dist %>/styles/fonts/{,*/}*.*',
           '<%= config.dist %>/*.{ico,png}'
         ]
